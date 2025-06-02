a6ee9ccebc8bba2aa8bcb8333725563a83d9faa3
John Gunnarsson <johngunnarsson@Johns-MacBook-Pro.local>
2025-06-02 16:42:09 +0200

    fix

--- src/main/java/com/spring/security/SecurityApplication.java
+++ src/main/java/com/spring/security/SecurityApplication.java
@@ -8,6 +8,6 @@ public class SecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello World!");
 	}
 }
